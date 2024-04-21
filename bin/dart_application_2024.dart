f9a27db594306ae27a5e4029d497fd4277cadba1
AndroPyro28 <menandroeugenio1028@gmail.com>
2024-04-21 14:44:20 +0800

    dart first commit

---
+++ bin/dart_application_2024.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+import 'package:dart_application_2024/dart_application_2024.dart' as dart_application_2024;
+
+void main(List<String> arguments) {
+  String name = "John Doe";
+  print(name);
+  bool isContained = name.contains("J");
+    dynamic dynamicVar = 'hello world';
+    dynamicVar = 1;
+    dynamicVar = true;
+  int num1 = 4;
+  double num2 = 11.5;
+
+  bool isEvenAndPositive = num1.isEven && !num2.isNegative ;
+
+  String? myString = null;
+  // myString = "hello world";
+  myString?.length;
+
+  print('${num2} ${num1} ${isEvenAndPositive} ${dynamicVar} ${myString?.length}');
+}
